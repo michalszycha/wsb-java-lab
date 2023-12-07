16d99d4d78efe735f68663943b4a004684d59e2e
michalszycha <michalsz378@o2.pl>
2023-12-07 22:38:23 +0100

    zad 2

--- src/main/java/com/example/GradeEvaluator.java
+++ src/main/java/com/example/GradeEvaluator.java
@@ -3,6 +3,9 @@ package com.example;
 import com.example.evaluation.Student;
 import com.example.evaluation.Teacher;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GradeEvaluator {
 
     public static void main(String[] args) {
@@ -11,6 +14,14 @@ public class GradeEvaluator {
 
         System.out.println(student.getFullName());
         System.out.println(teacher.getFullName());
+
+        student.addGrade(4);
+        student.addGrade(4);
+        student.addGrade(4);
+        student.addGrades(List.of(4, 4, 3));
+
+        System.out.println(student.getGrades());
+        System.out.println(student.getMeanGrades());
     }
 
 }
--- src/main/java/com/example/evaluation/Student.java
+++ src/main/java/com/example/evaluation/Student.java
@@ -1,8 +1,14 @@
 package com.example.evaluation;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private final String firstName;
     private final String lastName;
+
+    private final List<Integer> grades = new ArrayList<>();
 
     public Student(String firstName, String lastName) {
         this.firstName = firstName;
@@ -19,5 +25,24 @@ public class Student {
 
     public String getFullName() {
         return String.format("%s %s", firstName, lastName);
+    }
+
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    public void addGrades(List<Integer> grades) {
+        this.grades.addAll(grades);
+    }
+
+    public List<Integer> getGrades() {
+        return grades;
+    }
+
+    public double getMeanGrades() {
+        if(grades.isEmpty())
+            return 0;
+        int sum = grades.stream().mapToInt(Integer::intValue).sum();
+        return (double) sum / (double) grades.size();
     }
 }
